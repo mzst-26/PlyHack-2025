f721efdf0bc4528ad0c823a67a0a7df9b3f48702
mobin zaki <1380765@cityplym.ac.uk>
2025-01-25 16:21:23 +0000

    Sidebar Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
-import { Button } from "@/components/ui/button";
-import Playlists from "@/components/ui/data management/getserverdata";
-import Image from "next/image";
+"use client"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { useState } from "react";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Button>Click me</Button>
-      <Playlists/>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+  const [open, setOpen] = useState(true);
+  const isMobile = useIsMobile();
 
-      </footer>
-    </div>
+  return (
+    <SidebarProvider open={open} onOpenChange={setOpen}>
+      <AppSidebar />
+      <main className="max-w-full overflow-hidden">
+        {!isMobile && <SidebarTrigger/>}
+      </main>
+      
+    </SidebarProvider>
   );
 }
